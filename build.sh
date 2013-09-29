532d2f1c0fdf836f793be3bf40a90ca2b8a6d862
alekhin0w <alekhin0w@gmail.com>
2013-09-29 02:10:54 -0300

    cleaning binaries and processed files

--- build.sh
+++ build.sh
@@ -3,3 +3,4 @@ ca65 src/crt0.s
 ca65 src/famitraxx.s
 ca65 src/neslib.s
 ld65 -o famitraxx.nes -C src/nes.cfg src/crt0.o src/neslib.o src/famitraxx.o nes.lib 
+nes famitraxx.nes
--- srcasm/clean.sh
+++ srcasm/clean.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 rm asm6
 rm text2data
+rm nsf2data
 rm music.asm
 rm famitraxx.nes
